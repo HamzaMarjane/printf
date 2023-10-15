972ba373bf5cb32c2a34e6d0b3f79c7e149adee6
0soufiane <ess2003831@gmail.com>
2023-10-15 08:40:28 -0700

    betty-x

--- tests/_printf.c
+++ tests/_printf.c
@@ -1,57 +1,46 @@
 #include "main.h"
 /**
- * _printf - printing and handling formats.
- * @format : format string that we should manipulate.
- * 
- * Return : the count of the format string (its lenght).
+ * _printf - printf function for printing.
+ * @format: format string that we should manipulate.
+ *
+ * Return: int count of the format string (its lenght).
 */
-
 int _printf(const char *format, ...)
 {
-char c;
+va_list args;
+va_start(args, format);
 int count = 0;
-const char *p = format;
-va_list list;
-va_start(list,format);
-while (*p != '\0')
+for (char c, *p = format; *p; ++p)
 {
-	if (*p == '%')
-	{
-        p++;
-        count++;
-        switch (*p)
-        {
-        case 'c':
-        {
-            c = (char)va_arg(list,int);
-            putchar(c);
-            break;
-        }
-            case 's':
-            {
-            const char *str = va_arg(list,const char*);
-            while (*str != '\0')
-            {
-                putchar(*str);
-                str++;
-                count++;
-            }
-            break;
-            }
-            case '%':
-            {
-                putchar('%');
-                count++;
-            }
-               default:
-            break;
-        }
-    }
-           else{
-            putchar(*p);
-            count++;
-        }
-        p++;
+if (*p == '%')
+{
+++p;
+switch (*p)
+{
+case 'c':
+c = va_arg(args, int);
+putchar(c);
+++count;
+break;
+case 's':
+for (const char *str = va_arg(args, const char*); *str; ++str)
+{
+putchar(*str);
+++count;
 }
-        return (count);
+break;
+case '%':
+putchar('%');
+++count;
+break;
+}
+}
+else
+{
+putchar(*p);
+++count;
+}
+}
+va_end(args);
+return (count);
 }
