497e9edb78e53695c577809fe82ffad921c0a84b
HamzaMarjane <hamza.marjanee@gmail.com>
2023-10-15 07:43:57 -0700

    Fixingbetty

--- tests/_printf.c
+++ tests/_printf.c
@@ -1,84 +1,57 @@
 #include "main.h"
+/**
+ * _printf - printing and handling formats.
+ * @format : format string that we should manipulate.
+ * 
+ * Return : the count of the format string (its lenght).
+*/
 
 int _printf(const char *format, ...)
 {
 char c;
-int d;
-int i;
-int count =0;
-const char* p = format;
+int count = 0;
+const char *p = format;
 va_list list;
 va_start(list,format);
-
 while (*p != '\0')
 {
-    if (*p == '%')
-    {
+	if (*p == '%')
+	{
         p++;
         count++;
-            switch (*p)
+        switch (*p)
+        {
+        case 'c':
+        {
+            c = (char)va_arg(list,int);
+            putchar(c);
+            break;
+        }
+            case 's':
             {
-            case 'c':
+            const char *str = va_arg(list,const char*);
+            while (*str != '\0')
             {
-                c = (char)va_arg(list,int);
+                putchar(*str);
+                str++;
                 count++;
-                putchar(c);
-                break;
             }
-                case 's':
-                {
-                    const char *str = va_arg(list, const char *);
-                    while (*str != '\0') {
-                        putchar(*str);
-                        str++;
-                        count++;
-                    
-                    }
-                    break;
-                }
-                    case '%':
-                    {
-                        putchar('%');
-                        count++;
-                        break;
-                    }
-
-                     case 'd':
+            break;
+            }
+            case '%':
             {
-                d = va_arg(list,int);
+                putchar('%');
                 count++;
-                putchar(d + '0');
-                break;
             }
-
-            case 'i':
-            {
-                i = va_arg(list,int);
-                count++;
-                putchar(i + '0');
-                break;
-            }
-
-
-
-
-                
-            
-            default:
-                break;
-            }       
-
+               default:
+            break;
+        }
     }
-    else{
-        putchar(*p);
-        count++;
-    }
-    p++;
+           else{
+            putchar(*p);
+            count++;
+        }
+        p++;
 }
-va_end(list);
-return count;
-
-
-
-
+        return (count);
 }
